c29d11e2d6c6732661a1490f0e048d1b5bc7c6ad
zeyadhatem3 <ziadhatem3@gmail.com>
2023-11-14 17:38:57 +0100

    first commit

---
+++ src/main/java/me/hatem/recipeProject/RecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package me.hatem.recipeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeProjectApplication.class, args);
+	}
+
+}
